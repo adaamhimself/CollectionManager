b2dded407ffc3c9bb668170e336279f5d07fd745
Adam Keeling <69362306+adaamhimself@users.noreply.github.com>
2022-02-07 00:56:36 -0500

    Update collection.service.ts

--- collection-manager/src/app/collection.service.ts
+++ collection-manager/src/app/collection.service.ts
@@ -14,7 +14,7 @@ export class CollectionService {
   constructor(private http: HttpClient, private authService: AuthService) { }
 
   getCollectionById(id: String): Observable<any> { 
-    return this.http.get<Collection>(`${environment.apiBaseUrl}/collection/getCollectionById/${id}`);
+    return this.http.get<Collection>(`${environment.apiBaseUrl}collection/getCollectionById/${id}`);
   }
   
   getCollectionByUserId(): Observable<any> { 
@@ -22,14 +22,14 @@ export class CollectionService {
   }
 
   createCollection(newCollection: Collection): Observable<any> { 
-    return this.http.post<any>(`${environment.apiBaseUrl}/collection/createCollection`, newCollection);
+    return this.http.post<any>(`${environment.apiBaseUrl}collection/createCollection`, newCollection);
   }
   
   editCollection(editedCollection: Collection): Observable<any> { 
-    return this.http.post<any>(`${environment.apiBaseUrl}/collection/editCollection/`, editedCollection);
+    return this.http.post<any>(`${environment.apiBaseUrl}collection/editCollection/`, editedCollection);
   }
     
   removeCollection(id: String): Observable<any> { 
-    return this.http.delete<any>(`${environment.apiBaseUrl}/collection/removeCollection/${id}`);
+    return this.http.delete<any>(`${environment.apiBaseUrl}collection/removeCollection/${id}`);
   } 
 }
