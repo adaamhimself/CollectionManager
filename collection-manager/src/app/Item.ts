a72542154569a096c01bccdb0d40244dba6dc177
Samrudh-S <69688540+Samrudh-S@users.noreply.github.com>
2022-02-13 18:53:01 -0500

    Item Data Service

---
+++ collection-manager/src/app/Item.ts
@@ -0,0 +1,13 @@
+// Holds item objects
+
+export class Item {
+    "_id": string;
+    "item_name": string;
+    "item_description": string;
+    "item_user_id": String;
+    "item_image": 
+    {
+            "item_image_path": string;
+            "item_image_alt_text": string;
+    }
+}
