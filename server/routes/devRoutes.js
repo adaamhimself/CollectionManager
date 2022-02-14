621aa798770433e43d4f3dda2c4d9fdf40daa386
Adam Keeling <69362306+adaamhimself@users.noreply.github.com>
2022-02-14 10:57:41 -0500

    cleanup

--- server/routes/devRoutes.js
+++ server/routes/devRoutes.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const dev = require('../controllers/devController');
+const validate = require('../validateRequest');
 
-<<<<<<< Updated upstream
-router.post('/removeUser', passport.authenticate('jwt', {session: false}), async(req, res) => {
-=======
 router.post('/removeUser', validate.isAdmin, validate.validateRequestBody, passport.authenticate('jwt', {session: false}), async(req, res) => {
->>>>>>> Stashed changes
     let response = await dev.removeUser(req.body.email, req.user._id);
     res.status(response.code).json(response.message);
 });
