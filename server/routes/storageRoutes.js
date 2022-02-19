59dca77e6bc1c072de3acc10ea3cef3ee5e3017e
Adam Keeling <69362306+adaamhimself@users.noreply.github.com>
2022-02-18 19:05:02 -0500

    Update storageRoutes.js

--- server/routes/storageRoutes.js
+++ server/routes/storageRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const storage = require('../controllers/storageController');
+const validate = require('../validateRequest');
 
 router.get('/getItemsInStorageByCode/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
     let response = await storage.getItemsInStorageByCode(req.params.id);
