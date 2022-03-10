1f405f9b55a09760aa7881738b5938ebd4419e98
Julian <51174341+Phos4usOxide@users.noreply.github.com>
2022-03-10 18:30:36 -0500

    Article List improvements

--- collection-manager/src/app/article-list/article-list.component.ts
+++ collection-manager/src/app/article-list/article-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Article } from '../Article';
 import { ArticleService } from '../article.service';
+import { AuthService } from '../auth.service';
 
 @Component({
     selector: 'app-article-list',
@@ -10,17 +11,22 @@ import { ArticleService } from '../article.service';
 export class ArticleListComponent implements OnInit {
     public articles: Array<Article> = [];
     public warning: string;
+    public searchError = "";
+    public loadMessage = "Loading...";
+    public role = "";
+    public query = "";
     private articleSub: any = null;
+    private searchSub: any = null;
+    private roleSub: any = null;
 
-    constructor(private articleService: ArticleService) { }
+    constructor(private articleService: ArticleService, private auth: AuthService) { }
 
     ngOnInit(): void {
-        //retrieve all articles
-        this.articleSub = this.articleService.getListOfArticles().subscribe(
+        this.showArticles();
+        //get the user's role so "add article" can only be seen by admin users
+        this.roleSub = this.auth.isAdministrator().subscribe(
             (response) => {
-                if (response && response.length > 0){
-                    this.articles = response;
-                }
+                this.role = response;
             },
             (error) => {
                 this.warning = error.error;
@@ -28,8 +34,49 @@ export class ArticleListComponent implements OnInit {
         );
     }
 
-    //unsubscribes upon being destroyed
+    showArticles(): void {
+        //retrieve all articles
+        this.articleSub = this.articleService.getListOfArticles().subscribe(
+            (response) => {
+                this.articles = response;
+            },
+            (error) => {
+                this.warning = error.error;
+            }
+        );
+    }
+
+    //called when user clicks the "search" button
+    search(): void {
+        if (this.query.length == 0){
+            this.showArticles();//no query; show all
+        }
+        else if (this.query.length <= 2){
+            this.searchError = "Search query is too small";
+        } else if (this.query.length > 50) {
+            this.searchError = "Search query is too long";
+        } else {
+            this.searchError = "";
+            this.loadMessage = "Loading..."
+            this.articles = [];
+            //retrieve only articles that match the search query (title, category, etc.)
+            /*this.searchSub = this.articleService.searchArticles(this.query).subscribe(
+                (response) => {
+                    if (response.lenth == 0) this.loadMessage = "No articles found with that query"
+                    else this.articles = response;
+                },
+                (error) => {
+                    this.warning = error.error;
+                }
+            );*/
+        }
+        
+    }
+
+    //unsubscribe upon being destroyed
     ngOnDestroy() {
         if (this.articleSub) this.articleSub.unsubscribe();
+        if (this.searchSub) this.articleSub.unsubscribe();
+        if (this.roleSub) this.roleSub.unsubscribe();
     }
 }
