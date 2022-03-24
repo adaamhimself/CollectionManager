642fec2925fd660a1808b397bcd9010384a21708
Adam Keeling <69362306+adaamhimself@users.noreply.github.com>
2022-03-24 13:42:31 -0400

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const passport = require('passport');
 const user = require('../controllers/userController.js');
 
+router.get('/findUser', passport.authenticate('jwt', {session: false}), async(req, res) => {
+    let response = await user.findUser(req.body);
+    res.status(response.code).json(response.message);
+});
+
 router.get('/getuserDetails/:id', passport.authenticate('jwt', {session: false}), async(req, res) => {
     let response = await user.getUserDetails(req.params.id);
     res.status(response.code).json(response.message);
@@ -17,5 +22,6 @@ router.put('/editUserDetails/:id', passport.authenticate('jwt', {session: false}
     let response = await user.editUserDetails(req.params.id, req.body);
     res.status(response.code).json(response.message);
 });
+
 
 module.exports = router;
