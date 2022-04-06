c141268f1a1632a6073496c6f51bde7bea2e8a6c
Samrudh-S <69688540+Samrudh-S@users.noreply.github.com>
2022-04-05 21:59:38 -0400

    Edit Storage Update

--- collection-manager/src/app/edit-storage/edit-storage.component.ts
+++ collection-manager/src/app/edit-storage/edit-storage.component.ts
@@ -8,9 +8,12 @@ import { StorageService } from '../storage.service';
   templateUrl: './edit-storage.component.html',
   styleUrls: ['./edit-storage.component.css']
 })
-export class EditStorageComponent implements OnInit {
-  public warning: string;
-  private editSub: any;
+export class EditStorageComponent implements OnInit {  
+
+  public warning: string;  
+  public response: string;   
+  private editSub: any = null;
+
   public storageModel: Storage = new Storage;//synced form model
   public storageName: string = "";//displays at top of page
 
@@ -20,33 +23,33 @@ export class EditStorageComponent implements OnInit {
     let id: string = this.route.snapshot.params['id'];
     this.editSub = this.stoService.getStorageById(id).subscribe(
       (response) => {
-        this.storageModel = response;
+        this.storageModel = response;         
         if (this.storageModel) this.storageName = `${this.storageModel.storage_name}`;
-        console.log("item:", this.storageModel);
+          //console.log("item:", this.storageModel);
       }, (error) => {
           this.warning = error.error;
     }
     )
-}
-
-onSubmit(): void { 
-  console.log("Sending:", this.storageModel);
-    this.editSub = this.stoService.editStorageDetails(this.storageModel).subscribe(
-        response => {
-            console.log(response);
-        }, error => {
-            this.warning = error.error;
-        }
-    );
-    this.routing.navigate(['/managestorage']);
-}
-
-onClose(): void {
-    this.routing.navigate(['/managestorage']);
-}
-
-//unsubscribes upon being destroyed
-ngOnDestroy() {
-    if (this.editSub) this.editSub.unsubscribe();
   }
-}
+
+  onSubmit(): void { 
+    this.editSub = this.stoService.editStorageDetails(this.storageModel).subscribe(
+          (response) => {
+              console.log(response); 
+          }, error => {
+              this.warning = error.error;
+          }
+      ); 
+      // window.location.reload();
+      this.routing.navigate(['/managestorage']);    
+  }
+
+  onClose(): void {
+      this.routing.navigate(['/managestorage']);
+  }
+
+  //unsubscribes upon being destroyed
+  ngOnDestroy() {
+      if (this.editSub) this.editSub.unsubscribe();
+    }
+  }
