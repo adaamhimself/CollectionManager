eec720f3177efe83ace0872433e672942cb6a6dc
Samrudh-S <69688540+Samrudh-S@users.noreply.github.com>
2022-04-08 19:07:54 -0400

    Chat createConversation change, delete fix + duplicate/self validation

--- collection-manager/src/app/chat-home/chat-home.component.ts
+++ collection-manager/src/app/chat-home/chat-home.component.ts
@@ -21,39 +21,33 @@ export class ChatHomeComponent implements OnInit {
   // subscriptions
   private messageSub: any;
   private sendSub: any;
-  private repliesSub: any;
-
-  userMessage: string;
-  messages: any;
+  private repliesSub: any;  
+  private userSub: any ;
+  
   author: any;
   author_id: any;
   current_conversation_id: any;
   reply_body: any;
   other_participant_id: any;
+  userId: any;
+  userName: any;
 
   // Search Users
-  public users:Array<UserDetails> = [];//currently showing
-  public gridColumns = 4;
-
-  public messageTitle = "";
-  public message = "";
-  public query = "";  
-  public userCount = 0;
-  private userSub: any = null;
-
+  public users:Array<UserDetails> = []; // currently showing
+  public gridColumns = 3; 
+  public query = "";   
 
   constructor(private routing: Router, private route: ActivatedRoute, private conversationService: ConversationService, private userService:UserService) { }
 
   ngOnInit(): void {
     this.messageSub = this.conversationService.getConversationList().subscribe(
       (response) => {
-        console.log(this.conversations);
         this.conversations = response;
         this.author = this.conversations[0].username;
         this.author_id = this.conversations[0].other_participant_id;
         this.current_conversation_id = this.conversations[0]._id;
         this.other_participant_id = this.conversations[0].other_participant_id;
-        this.loadMessages(this.conversations[0]._id, this.author, this.other_participant_id);
+        this.loadMessages(this.conversations[0]._id, this.author, this.other_participant_id);      
       },
       (error) => {
         this.warning = error.error;
@@ -61,13 +55,22 @@ export class ChatHomeComponent implements OnInit {
       }
     );
 
+    this.userSub = this.userService.getMyDetails().subscribe(
+      (response) => {
+        this.userId = response._id; 
+        this.userName = response.username;
+      }, 
+      (error) => {
+        this.warning = error.error;
+        console.log(error);
+      }
+    );
   }
 
   sendMessage(): void {
     this.sendSub = this.conversationService.addToConversation(this.current_conversation_id, this.reply_body).subscribe(
       (response) => {
-        this.reply_body = "";
-        console.log(this.current_conversation_id, this.reply_body);
+        this.reply_body = ""; 
         this.loadMessages(this.current_conversation_id, this.author, this.other_participant_id);
       },
       (error) => {
@@ -75,71 +78,69 @@ export class ChatHomeComponent implements OnInit {
       }
     );
   }
+
   //update the shown users using the search query
-  search(): void {
-    if (this.query.length == 0){
-        this.messageTitle = "Search Users"
-        this.message = "Please provide a query before searching";
-        return;
-      }
-    this.messageTitle = "Searching..."
-    this.message = "Users that fit your query will appear below";
-    //get all users that fit the query
+  search(): void { 
     this.userSub = this.userService.findUser(this.query).subscribe(
         (response) => {
-            this.users = response;
-            //update the count
-            this.userCount = this.users.length;
-            //update the message depending on whether there were any returned users
-            if (this.userCount == 0){
-                this.messageTitle = "No Users";
-                this.message = "No users fit that search query. Check spelling or try a simpler query."
-            } else {
-                this.messageTitle = "Search Users";
-                this.message = "Users that fit your query will appear below";
-            }
+          this.users = response;           
         },
         (error) => {
-            this.warning = error.error;
-            //update message when there's an error (back to default)
-            this.messageTitle = "Search Users";
-            this.message = "Users that fit your query will appear below";
+          this.warning = error.error;           
         }
     );
   }
 
-  createConversation(id, message): void {
-    this.sendSub = this.conversationService.createConversation(id, message).subscribe(
-      (response) => {
-        window.location.reload();
-      },
-      (error) => {
-        this.warning = error.error;
-      }
-    );
-  }
+  createConversation(id): void {
+    var flag: boolean = false;
+
+    // Checking for duplicates and conversations with self
+    for (var conversation of this.conversations)
+    {
+      if (conversation.other_participant_id == id || this.userId == id) {
+        flag = true;
+      }   
+    }
+    if (flag == false) {
+      this.sendSub = this.conversationService.createConversation(id).subscribe(
+        (response) => {
+          window.location.reload();
+          this.userName = this.sendSub.username;
+        },
+        (error) => {
+          this.warning = error.error;
+        }
+      ); 
+      console.log("userId",this.userId);
+      console.log("userName",this.sendSub.userName);
+      console.log("Id", id);
+      this.loadMessages(this.userId, this.userName, id);
+    }    
+  }    
 
   loadMessages(id, username, other_participant): void {
+    console.log("userId", id);
+    console.log("userName", username);
+    console.log("Other Id", other_participant);
     this.current_conversation_id = id;
     this.other_participant_id = other_participant;
     this.author = username;
     this.repliesSub = this.conversationService.getMessagesWithUser(this.other_participant_id).subscribe(
       (response) => {
-        this.replies = response.messages;
-        console.log(this.replies);
-      },
+        this.replies = response.messages; 
+       },
       (error) => {
         this.warning = error.error;
       }
-    );
+    ); 
   }
 
-  onDelete(): void {
-    this.messageSub = this.conversationService.deleteConversation(this.current_conversation_id).subscribe(
+  onDelete(id): void {
+    this.messageSub = this.conversationService.deleteConversation(id).subscribe(
       (response) => {
           console.log(response);
           console.log("successfully deleted conversation");
-          window.location.reload();//reload the page
+          window.location.reload(); // reload the page
       },
       (error) => {
           console.log("failed deleting conversation");
